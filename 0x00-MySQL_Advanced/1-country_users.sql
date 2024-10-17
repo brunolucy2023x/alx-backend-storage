015386e552b94838e8a05d9265809b7986ec5819
brunox327 <lucyb5147@gmail.com>
2024-10-17 11:43:46 +0000

    trial six

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,13 +1,10 @@
--- Creates a table users with below attributes
--- id, integer, never null, auto increment and primary key
--- email, string (255 characters), never null and unique
--- name, string (255 characters)
--- country, enum of countries: US, CO and TN, never null (default=US)
--- If table exists, script will not fail, can be executed on any database
+-- Author: Bruno Owino
+-- Creates a users table with an auto-incrementing ID, a unique email, and a country field with default value US.
+-- The script is safe to run on any database, as it won’t fail if the table already exists.
 
 CREATE TABLE IF NOT EXISTS users (
        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
        email VARCHAR(255) NOT NULL UNIQUE,
        name VARCHAR(255),
-       country ENUM ('US', 'CO', 'TN') NOT NULL
+       country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
