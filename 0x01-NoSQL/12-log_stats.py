2f4620f8e501eb7597c58b395d8ad7d7344a1b81
brunolucy2023x <144634638+brunolucy2023x@users.noreply.github.com>
2024-10-23 10:08:43 +0300

    12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 """
-Provide some stats about Nginx logs stored in MongoDB
-Database: logs, Collection: nginx, Display same as example
-first line: x logs, x number of documents in this collection
-second line: Methods
-5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-one line with method=GET, path=/status
+Provide statistics about Nginx logs stored in MongoDB
+Database: logs, Collection: nginx, Display format as shown in the example
+First line: x logs, x number of documents in this collection
+Second line: Methods
+Five lines with methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+One line with method=GET, path=/status
+Author: Bruno Owino
 """
 from pymongo import MongoClient
 
-
 METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
 
 def log_stats(mongo_collection, option=None):
     """
     Prototype: def log_stats(mongo_collection, option=None):
-    Provide some stats about Nginx logs stored in MongoDB
+    Provide statistics about Nginx logs stored in MongoDB
+    Author: Bruno Owino
     """
     items = {}
     if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
+        value = mongo_collection.count_documents({"method": {"$regex": option}})
         print(f"\tmethod {option}: {value}")
         return
 
@@ -29,10 +28,9 @@ def log_stats(mongo_collection, option=None):
     print(f"{result} logs")
     print("Methods:")
     for method in METHODS:
-        log_stats(nginx_collection, method)
+        log_stats(mongo_collection, method)  # Corrected to use mongo_collection
     status_check = mongo_collection.count_documents({"path": "/status"})
     print(f"{status_check} status check")
-
 
 if __name__ == "__main__":
     nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
