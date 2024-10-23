afa40fe1736799cd5919af52c3686cb3fef21d3a
brunolucy2023x <144634638+brunolucy2023x@users.noreply.github.com>
2024-10-23 09:58:07 +0300

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 """
-List all documents in a collection
+Retrieve all documents in a collection
 Prototype: def list_all(mongo_collection):
-Return an empty list if no document in the collection
+Return an empty list if there are no documents in the collection
 mongo_collection will be the pymongo collection object
+Author: Bruno Owino
 """
-
 
 def list_all(mongo_collection):
     """
     Prototype: def list_all(mongo_collection)
-    Return an empty list if no document in the collection
+    Return an empty list if there are no documents in the collection
+    Author: Bruno Owino
     """
     documents = mongo_collection.find()
-    return documents
+    return list(documents)  # Convert cursor to a list
