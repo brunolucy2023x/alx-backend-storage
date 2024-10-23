f2f4c4e874ddfd7e96b15bf7bbdb4424f3770439
brunolucy2023x <144634638+brunolucy2023x@users.noreply.github.com>
2024-10-23 10:01:01 +0300

    9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """
-Insert a new document in a collection based on kwargs
+Insert a new document into a collection based on keyword arguments
 Prototype: def insert_school(mongo_collection, **kwargs):
-mongo_collection will be pymongo collection object
-Returns new _id
+mongo_collection will be a pymongo collection object
+Returns the new _id
+Author: Bruno Owino
 """
-
 
 def insert_school(mongo_collection, **kwargs):
     """
     Prototype: def insert_school(mongo_collection, **kwargs):
     Returns the new _id
+    Author: Bruno Owino
     """
     new_document = mongo_collection.insert_one(kwargs)
     return new_document.inserted_id
+
