42ec2e393923ab49ec09277f46730c393147d5b2
brunolucy2023x <144634638+brunolucy2023x@users.noreply.github.com>
2024-10-23 10:03:01 +0300

    10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """
-Changes all topics of a schooll document based on the name
+Update all topics of a school document based on the name
 Prototype: def update_topics(mongo_collection, name, topics):
 mongo_collection will be the pymongo object
 name (String) will be the school name to update
-topics (list of strings) will be the list of topics approached in the school
+topics (list of strings) will be the list of topics covered in the school
+Author: Bruno Owino
 """
-
 
 def update_topics(mongo_collection, name, topics):
     """
     Prototype: def update_topics(mongo_collection, name, topics):
-    Changes all topics of a school document
+    Updates all topics of a school document
+    Author: Bruno Owino
     """
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+
